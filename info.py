d4a0c5ec3551ec80301d07fec695b60d590d770a
Luffy <120936732+anime91eki@users.noreply.github.com>
2023-08-31 18:28:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -14,7 +14,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '29467528'))
 API_HASH = environ.get('API_HASH', '0bda31de4693f3413d868ebdeb087940')
-BOT_TOKEN = environ.get('BOT_TOKEN', '6241767855:AAH04K5R2Bx-k5s6sfOG3KSTs-vRZ1J0Olo')
+BOT_TOKEN = environ.get('BOT_TOKEN', '6089249298:AAF0L9__xUWfkIIlBRvY5XyyE_nLAVG_o-0')
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
@@ -31,13 +31,13 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Abhi12:abhi12@cluster0.bdue3ap.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Shadow_Sanpai:shadowsanpai@cluster0.bvmudij.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "NoobXBotz")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001684388491'))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/movies_anime_zone')
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001802708543'))
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/Anime_Chat_Guardians')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "True")), False)
 SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "True")), True)
@@ -63,7 +63,7 @@ LOG_STR += ("Spell Check Mode Is Enabled, bot will be suggesting related movies 
 LOG_STR += (f"MAX_LIST_ELM Found, long list will be shortened to first {MAX_LIST_ELM} elements\n" if MAX_LIST_ELM else "Full List of casts and crew will be shown in imdb template, restrict them by adding a value to MAX_LIST_ELM\n")
 LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
 
-HOW_TO_DOWNLOAD =  environ.get('HOW_TO_DOWNLOAD', 'https://t.me/Sharath_Links/13')
+HOW_TO_DOWNLOAD =  environ.get('HOW_TO_DOWNLOAD', 'https://t.me/How_to_get_Files_AG')
 
 AUTO_DELETE_SECONDS = int(environ.get('AUTO_DELETE_SECONDS', 100))
 
